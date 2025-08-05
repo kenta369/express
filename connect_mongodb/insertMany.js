e6d4a1cd62548bd8250c6a12fcc8fcb19adde68c
kenta369 <kenk79017@gmail.com>
2025-08-05 19:25:03 +0900

    add cat api

--- connect_mongodb/insertMany.js
+++ connect_mongodb/insertMany.js
@@ -1,6 +1,6 @@
  const { MongoClient} = require("mongodb");
  // 下のURIをご自分の値に変更してください
- const uri="mongodb+srv://kenta:password1234@cluster0.4fft0rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ const uri = " mongodb+srv://kenta:password1234@cluster0.4fft0rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 ";
  const client= new MongoClient(uri);
  async function run() {
  const database = client.db('notes');
